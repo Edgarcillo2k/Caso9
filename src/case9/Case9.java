e6c2f49dd07c9707f80fb8b209b0cae9c0e740a1
Edgar <edgar.mata.1911@gmail.com>
2019-10-10 10:53:05 -0600

    RSA

--- src/case9/Case9.java
+++ src/case9/Case9.java
@@ -1,6 +1,16 @@
 package case9;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 public class Case9 
 {
-	public static void main(String[] args) {}
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+        RSA keyPairGenerator = new RSA();
+        keyPairGenerator.writeToFile("RSA/publicKey", keyPairGenerator.getPublicKey().getEncoded());
+        keyPairGenerator.writeToFile("RSA/privateKey", keyPairGenerator.getPrivateKey().getEncoded());
+        System.out.println(Base64.getEncoder().encodeToString(keyPairGenerator.getPublicKey().getEncoded()));
+        System.out.println(Base64.getEncoder().encodeToString(keyPairGenerator.getPrivateKey().getEncoded()));
+    }
 }
