1388b277d3233340a8fb87074b536ee286d62dd5
Edgar <edgar.mata.1911@gmail.com>
2019-10-17 05:58:50 -0600

    Sockets

--- src/case9/Case9.java
+++ src/case9/Case9.java
@@ -13,16 +13,5 @@ public class Case9
 {
 	public static void main(String[] args) throws IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, IOException {
         Window window = new Window();
-        /*
-		RSAUtil rsaUtil = new RSAUtil();
-		try {
-            String encryptedString = Base64.getEncoder().encodeToString(rsaUtil.encrypt("Dhiraj is the author"));
-            System.out.println(encryptedString);
-            String decryptedString = RSAUtil.decrypt(encryptedString);
-            System.out.println(decryptedString);
-        } catch (NoSuchAlgorithmException e) {
-            System.err.println(e.getMessage());
-        }
-        */
     }
 }
